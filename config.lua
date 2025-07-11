e7428091ab4c0a114ed58b3b92982f8dfecac3a5
FilPag <pagliarofilip@gmail.com>
2025-07-11 15:10:25 +0200

    updated config IP

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 return {
 	["username"] = "Guest",
 	["blind_col"] = 1,
-	["server_url"] = "localhost",
+	["server_url"] = "84.217.122.248",
 	["server_port"] = 8788,
 	["logging"] = false,
 	["misprint_display"] = true,
