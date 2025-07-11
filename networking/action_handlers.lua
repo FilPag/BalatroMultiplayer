009d5f35e1aef4fde586bb2ce24c2a0e4b664c21
FilPag <pagliarofilip@gmail.com>
2025-07-11 15:07:57 +0200

    update sync of scores

--- networking/action_handlers.lua
+++ networking/action_handlers.lua
@@ -130,10 +130,16 @@ local function begin_pvp_blind()
 end
 
 local function action_start_blind()
-        MP.GAME.ready_blind = false
-        MP.GAME.timer_started = false
-        MP.GAME.timer = MP.LOBBY.config.timer_base_seconds
-        MP.UI.start_pvp_countdown(begin_pvp_blind)
+	MP.GAME.ready_blind = false
+	MP.GAME.timer_started = false
+	MP.GAME.timer = MP.LOBBY.config.timer_base_seconds
+  MP.UI.start_pvp_countdown(begin_pvp_blind)
+
+	if MP.GAME.next_blind_context then
+		G.FUNCS.select_blind(MP.GAME.next_blind_context)
+	else
+		sendErrorMessage("No next blind context", "MULTIPLAYER")
+	end
 end
 
 local function action_ally_info(score_str, hand_left_str)
@@ -198,12 +204,21 @@ local function action_win_game()
 end
 
 local function action_lose_game()
-	MP.end_game_jokers_payload = ""
-	MP.nemesis_deck_string = ""
-	MP.end_game_jokers_received = false
-	MP.nemesis_deck_received = false
-	G.STATE_COMPLETE = false
-	G.STATE = G.STATES.GAME_OVER
+	G.E_MANAGER:add_event(Event({
+		no_delete = true,
+		trigger = "immediate",
+		blockable = false,
+		blocking = false,
+		func = function()
+			MP.end_game_jokers_payload = ""
+			MP.nemesis_deck_string = ""
+			MP.end_game_jokers_received = false
+			MP.nemesis_deck_received = false
+			G.STATE_COMPLETE = false
+			G.STATE = G.STATES.GAME_OVER
+			return true
+		end,
+	}))
 end
 
 -- Helper: parse option value by type
