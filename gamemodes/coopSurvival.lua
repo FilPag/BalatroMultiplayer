289d64b87d10de03499d0bdd1edf0503df2b493d
FilPag <pagliarofilip@gmail.com>
2025-07-15 21:25:23 +0200

    fixed issues with coop synching scores and blinds

--- gamemodes/coopSurvival.lua
+++ gamemodes/coopSurvival.lua
@@ -1,8 +1,12 @@
 MP.Gamemode({
     key = "coopSurvival",
     get_blinds_by_ante = function(self, ante, choices)
+
         if MP.LOBBY.isHost then
             MP.ACTIONS.set_Boss(choices.Boss)
+        elseif MP.next_coop_boss then
+            choices.Boss = MP.next_coop_boss
+            MP.next_coop_boss = nil
         end
 
         return choices.Small, choices.Big , choices.Boss 
