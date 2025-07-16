b68211d14de16cc7afa209cd1da16d7f97e027e3
FilPag <pagliarofilip@gmail.com>
2025-07-16 18:18:00 +0200

    fixed losing screen

--- ui/components/counters.lua
+++ ui/components/counters.lua
@@ -1,5 +1,9 @@
 function MP.UI.round_replacement()
-  return MP.UI.lives_counter()
+  local scale = 0.4
+  local temp_col = G.C.DYN_UI.BOSS_MAIN
+  local temp_col2 = G.C.DYN_UI.BOSS_DARK
+
+  return MP.UI.lives_counter(scale, temp_col, temp_col2)
 end
 
 function MP.UI.ante_replacement()
@@ -7,7 +11,10 @@ function MP.UI.ante_replacement()
     return MP.UI.timer_hud()
   end
 
-  return nil
+  local scale = 0.4
+  local temp_col = G.C.DYN_UI.BOSS_MAIN
+  local temp_col2 = G.C.DYN_UI.BOSS_DARK
+  return MP.UI.coop_ante_counter(scale, temp_col, temp_col2)
 end
 
 function MP.UI.timer_hud()
@@ -69,11 +76,7 @@ function MP.UI.timer_hud()
   }
 end
 
-function MP.UI.lives_counter()
-  local scale = 0.4
-  local temp_col = G.C.DYN_UI.BOSS_MAIN
-  local temp_col2 = G.C.DYN_UI.BOSS_DARK
-
+function MP.UI.lives_counter(scale, temp_col, temp_col2)
   return {
     n = G.UIT.C,
     config = { id = 'hud_ante', align = "cm", padding = 0.05, minw = 1.45, minh = 1, colour = temp_col, emboss = 0.05, r = 0.1 },
@@ -102,6 +105,29 @@ function MP.UI.lives_counter()
               id = 'ante_UI_count'
             }
           },
+        }
+      },
+    }
+  }
+end
+
+function MP.UI.coop_ante_counter(scale, temp_col, temp_col2)
+  return {
+    n = G.UIT.C,
+    config = { id = 'hud_ante', align = "cm", padding = 0.05, minw = 1.45, minh = 1, colour = temp_col, emboss = 0.05, r = 0.1 },
+    nodes = {
+      {
+        n = G.UIT.R,
+        config = { align = "cm", minh = 0.33, maxw = 1.35 },
+        nodes = {
+          { n = G.UIT.T, config = { text = localize('k_ante'), scale = 0.85 * scale, colour = G.C.UI.TEXT_LIGHT, shadow = true } },
+        }
+      },
+      {
+        n = G.UIT.R,
+        config = { align = "cm", r = 0.1, minw = 1.2, colour = temp_col2 },
+        nodes = {
+          { n = G.UIT.O, config = { object = DynaText({ string = { { ref_table = G.GAME.round_resets, ref_value = 'ante' } }, colours = { G.C.IMPORTANT }, shadow = true, font = G.LANGUAGES['en-us'].font, scale = 2 * scale }), id = 'ante_UI_count' } },
         }
       },
     }
