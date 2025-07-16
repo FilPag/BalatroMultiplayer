89467cfc89fb58d21f9616b8688159ef38f81cb6
FilPag <pagliarofilip@gmail.com>
2025-07-16 15:19:31 +0200

    fixed ante and lives counter

---
+++ ui/components/counters.lua
@@ -0,0 +1,109 @@
+function MP.UI.round_replacement()
+  return MP.UI.lives_counter()
+end
+
+function MP.UI.ante_replacement()
+  if MP.LOBBY.code and not MP.LOBBY.config.disable_live_and_timer_hud and not MP.UTILS.is_coop() then
+    return MP.UI.timer_hud()
+  end
+
+  return nil
+end
+
+function MP.UI.timer_hud()
+  return {
+    n = G.UIT.C,
+    config = {
+      align = "cm",
+      padding = 0.05,
+      minw = 1.45,
+      minh = 1,
+      colour = G.C.DYN_UI.BOSS_MAIN,
+      emboss = 0.05,
+      r = 0.1,
+    },
+    nodes = {
+      {
+        n = G.UIT.R,
+        config = { align = "cm", maxw = 1.35 },
+        nodes = {
+          {
+            n = G.UIT.T,
+            config = {
+              text = localize("k_timer"),
+              minh = 0.33,
+              scale = 0.34,
+              colour = G.C.UI.TEXT_LIGHT,
+              shadow = true,
+            },
+          },
+        },
+      },
+      {
+        n = G.UIT.R,
+        config = {
+          align = "cm",
+          r = 0.1,
+          minw = 1.2,
+          colour = G.C.DYN_UI.BOSS_DARK,
+          id = "row_round_text",
+          func = "set_timer_box",
+          button = "mp_timer_button",
+        },
+        nodes = {
+          {
+            n = G.UIT.O,
+            config = {
+              object = DynaText({
+                string = { { ref_table = MP.GAME, ref_value = "timer" } },
+                colours = { G.C.UI.TEXT_DARK },
+                shadow = true,
+                scale = 0.8,
+              }),
+              id = "timer_UI_count",
+            },
+          },
+        },
+      },
+    },
+  }
+end
+
+function MP.UI.lives_counter()
+  local scale = 0.4
+  local temp_col = G.C.DYN_UI.BOSS_MAIN
+  local temp_col2 = G.C.DYN_UI.BOSS_DARK
+
+  return {
+    n = G.UIT.C,
+    config = { id = 'hud_ante', align = "cm", padding = 0.05, minw = 1.45, minh = 1, colour = temp_col, emboss = 0.05, r = 0.1 },
+    nodes = {
+      {
+        n = G.UIT.R,
+        config = { align = "cm", minh = 0.33, maxw = 1.35 },
+        nodes = {
+          { n = G.UIT.T, config = { text = localize('k_lives'), scale = 0.85 * scale, colour = G.C.UI.TEXT_LIGHT, shadow = true } },
+        }
+      },
+      {
+        n = G.UIT.R,
+        config = { align = "cm", r = 0.1, minw = 1.2, colour = temp_col2 },
+        nodes = {
+          {
+            n = G.UIT.O,
+            config = {
+              object = DynaText({
+                string = { { ref_table = MP.UTILS.get_local_player(), ref_value = "lives" } },
+                colours = { G.C.IMPORTANT },
+                shadow = true,
+                font = G.LANGUAGES["en-us"].font,
+                scale = 2 * scale,
+              }),
+              id = 'ante_UI_count'
+            }
+          },
+        }
+      },
+    }
+  }
+end
