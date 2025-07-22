38fb87592f58dcb51f9cf10890aa81346979fb82
unknown <pagliarofilip@gmail.com>
2025-07-22 20:18:42 +0200

    fixed issues with continue in singleplayer

--- ui/lobby.lua
+++ ui/lobby.lua
@@ -1169,10 +1169,8 @@ function Game:update(dt)
 	if (MP.LOBBY.code and not in_lobby) or (not MP.LOBBY.code and in_lobby) then
 		in_lobby = not in_lobby
 		G.F_NO_SAVING = in_lobby
-		if true then -- G.STATE == G.STATES.MENU, revert if something breaks, but this causes disconnects to not exit the game
-			self.FUNCS.go_to_menu()
-			MP.reset_game_states()
-		end
+		self.FUNCS.go_to_menu()
+		MP.reset_game_states()
 	end
 	gameUpdateRef(self, dt)
 end
