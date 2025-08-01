600c6c27906ced369300b4af249cc9ad21000d2d
unknown <pagliarofilip@gmail.com>
2025-08-01 19:57:33 +0200

    fix with selecting deck

--- function_overrides/call_backs.lua
+++ function_overrides/call_backs.lua
@@ -1,7 +1,7 @@
 local exit_overlay_menu_ref = G.FUNCS.exit_overlay_menu
 G.FUNCS.exit_overlay_menu = function()
 
-  if MP.LOBBY.code and MP.LOBBY.is_host then
+  if MP.LOBBY.code and MP.LOBBY.is_host and G.OVERLAY_MENU then
     if G.OVERLAY_MENU:get_UIE_by_ID("lobby_options") then
       MP.ACTIONS.update_lobby_options()
     end
